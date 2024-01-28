4340bf6df7616faca96ad1665b9fc14d5038803e
Nikhil4147 <nikhil.waghmare4147@gmail.com>
2024-01-28 14:41:41 +0530

    pass commit message here

---
+++ variable.py
@@ -0,0 +1,10 @@
+x = 6  # integer number
+y = "python"  # string
+z = 10.4   # float number
+print("x data type is:- ", type(x))
+print("y data type is:- ", type(y))
+print("z data type is:- ", type(z))
+
+print(x)
+print(y)
+print(z)
